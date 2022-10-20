9fc2ebdb1918aacbb57a58e87155213a0dd45601
JacAul07 <jacaul07@gmail.com>
2022-10-20 08:54:41 +0200

    Created .net console backend
    Created A rudimentary .net backend as a console application and added a few classes.

---
+++ Backend Userclient/communicator/communicator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace communicator {
+
+    class McClient {
+
+        string name = "";
+        int players = 0;
+        int tps = 0;
+        int status = 0;
+
+    }
+
+    class Backend {
+
+        static void Main(string[] args) {
+            Console.WriteLine("Initializing backend...");
+        }
+
+    }
+}
