4d26cc5ce61a0d9a8288ac9585f63c0ecf2cc89b
RedTechTiger <jacaul07@gmail.com>
2023-01-05 19:29:47 +0000

    Sync repos

--- Swirve Framework/logger/log.cpp
+++ Swirve Framework/logger/log.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include "log.h"
 
-#define MSG_0 "\u001b[32mOK\u001b[37m"
-#define MSG__ "\u001b[31mFAIL\u001b[37m"
+#define MSG_0 "\u001b[32mOK\u001b[0m"
+#define MSG__ "\u001b[31mFAIL\u001b[0m"
 
 using namespace std;
 
@@ -15,14 +15,15 @@ void Logger::unsetDebug() {
 }
 
 void Logger::logFunction(const string msg, const int ret) {
-    cout << msg << " [" << (ret == 0 ? MSG_0 : MSG__) << "\n";
+    cout << msg << " [" << (ret == 0 ? MSG_0 : MSG__) << "]\n";
     fflush(stdout);
 }
 
 void Logger::logLengthyFunction(const string msg) {
     cout << "[*] " << msg << " ";
     fflush(stdout);
-    logging = 1;
 }
 
-void Logger::logFinish(const int ret);
+void Logger::logFinish(const int ret) {
+    cout << "[" << (ret == 0 ? MSG_0 : MSG__) << "]\n";
+}
